839ed0b37299f1f7662e767aca3bcfe562f476fb
Syaz S <syazski@gmail.com>
2024-02-26 17:52:29 -0600

    register form

--- ecommerce-site/src/components/Register.jsx
+++ ecommerce-site/src/components/Register.jsx
@@ -2,7 +2,20 @@ function Register() {
 
     return (
       <>
-      <h2>Register</h2>
+      <h2>Register to access books</h2>
+      <form>
+        <label htmlFor="username">Username: </label>
+        <input type="text" name="username"/><br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password"/><br />
+        <p>More about you</p>
+        <label htmlFor="firstname">First Name: </label>
+        <input type="text" name="firstname"/><br />
+        <label htmlFor="lastname">Last Name: </label>
+        <input type="text" name="lastname"/><br />
+        <br/>
+        <button>Submit</button>
+      </form>
       </>
     )
   }
