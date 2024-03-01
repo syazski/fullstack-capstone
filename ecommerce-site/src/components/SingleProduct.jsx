f51218cff164a77541c4ba9d5385bbb3ec5f29ea
Syaz S <syazski@gmail.com>
2024-03-01 11:59:13 -0600

    single product page

--- ecommerce-site/src/components/SingleProduct.jsx
+++ ecommerce-site/src/components/SingleProduct.jsx
@@ -1,3 +1,4 @@
+import "../index.css"
 import { useProductDetailsQuery } from '../redux/api';
 import { useParams } from 'react-router-dom';
 
@@ -19,24 +20,22 @@ function SingleProduct() {
                 <>
                 <h2>Product Details</h2>
                 <div className ="single-products">
-                { data.map((product) => {
-                  return (
-                        <div key={product.id} className="singleproduct-card">
-                        <img src={product.image} alt={product.title} className="singleproduct-image" />
+                {/* { data.map((product) => { */}
+                        <div key={data.id} className="singleproduct-card">
+                        <img src={data.image} alt={data.title} className="singleproduct-image" />
                         
                         <div className="singleproduct-details">
-                        <p><strong>Item:</strong> {product.title}</p>
-                        <p><strong>Description:</strong> {product.description}</p>
-                        <p><strong>Category:</strong> {product.category}</p>
-                        <p><strong>Price:</strong>${product.price}</p>
-                        <p><strong>Rating:</strong> {product.rating.rate}</p>
+                        <p><strong>Item:</strong> {data.title}</p>
+                        <p><strong>Description:</strong> {data.description}</p>
+                        <p><strong>Category:</strong> {data.category}</p>
+                        <p><strong>Price:</strong>${data.price}</p>
+                        <p><strong>Rating:</strong> {data.rating.rate}</p>
                         <br />
                         <button>Add to Cart</button>
                     </div>
                     </div>
-                )
-            })
-            }
+            {/* })
+            } */}
         </div>
         </>
         )
