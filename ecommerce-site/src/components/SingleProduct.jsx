30c683902759a1ce1a65003256f7e245f7b16445
Syaz S <syazski@gmail.com>
2024-03-11 17:41:21 -0500

    removed add to cart for non-logged in users

--- ecommerce-site/src/components/SingleProduct.jsx
+++ ecommerce-site/src/components/SingleProduct.jsx
@@ -2,7 +2,7 @@ import "../index.css"
 import { useProductDetailsQuery } from '../redux/api';
 import { useParams } from 'react-router-dom';
 
-function SingleProduct() {
+function SingleProduct(props) {
         let { id } = useParams();
         const { data, error, isLoading } = useProductDetailsQuery({id});
         
@@ -15,12 +15,11 @@ function SingleProduct() {
 
         //console.log(id);
         console.log(data);
-        
+        if(props.token) {
         return ( 
                 <>
                 <h2>Product Details</h2>
                 <div className ="single-products">
-                {/* { data.map((product) => { */}
                         <div key={data.id} className="singleproduct-card">
                         <img src={data.image} alt={data.title} className="singleproduct-image" />
                         
@@ -34,11 +33,31 @@ function SingleProduct() {
                         <button>Add to Cart</button>
                     </div>
                     </div>
-            {/* })
-            } */}
-        </div>
-        </>
+                </div>
+                </>
         )
+} else {
+        return ( 
+                <>
+                <h2>Product Details</h2>
+                <div className ="single-products">
+                        <div key={data.id} className="singleproduct-card">
+                        <img src={data.image} alt={data.title} className="singleproduct-image" />
+                        
+                        <div className="singleproduct-details">
+                        <p><strong>Item:</strong> {data.title}</p>
+                        <p><strong>Description:</strong> {data.description}</p>
+                        <p><strong>Category:</strong> {data.category}</p>
+                        <p><strong>Price:</strong>${data.price}</p>
+                        <p><strong>Rating:</strong> {data.rating.rate}</p>
+                        <br />
+                        <button>Login to Add to Cart</button>
+                    </div>
+                    </div>
+                </div>
+                </>
+        )
+}
 }
 
 export default SingleProduct;
