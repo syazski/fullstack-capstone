7ae3fdf4ac335ac06dda0cc2c2d1c4e7464eec42
Syaz S <syazski@gmail.com>
2024-03-18 18:53:22 -0500

    format with prettier

--- ecommerce-site/src/App.jsx
+++ ecommerce-site/src/App.jsx
@@ -1,15 +1,15 @@
-import './index.css';
-import shop from './assets/icon.png'
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
-import Account from './components/Account';
-import Cart from './components/Cart';
-import Nav from './components/Navbar';
-import Checkout from './components/Checkout';
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SingleProduct from './components/SingleProduct';
+import "./index.css";
+import shop from "./assets/icon.png";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Account from "./components/Account";
+import Cart from "./components/Cart";
+import Nav from "./components/Navbar";
+import Checkout from "./components/Checkout";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingleProduct from "./components/SingleProduct";
 
 function App() {
   const [user, setUser] = useState();
@@ -18,30 +18,63 @@ function App() {
   const [products, setProducts] = useState(null);
   const [cartItems, setCartItems] = useState([]);
   const [userId, setUserId] = useState(null);
-  
+
   //console.log("Cart Items:", cartItems)
-  
+
   return (
     <div>
-    <div className="logo">
-    <h1><img id='logo-image' src={shop}/>Buymorestuff.com</h1>
+      <div className="logo">
+        <h1>
+          <img id="logo-image" src={shop} />
+          Buymorestuff.com
+        </h1>
+      </div>
+      <BrowserRouter>
+        <nav>
+          <Nav token={token} setToken={setToken} userId={userId} />
+        </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                token={token}
+                setProducts={setProducts}
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+              />
+            }
+          />
+          <Route path="/register" element={<Register setId={setId} />} />
+          <Route
+            path="/login"
+            element={<Login setToken={setToken} setUser={setUser} />}
+          />
+          <Route
+            path="/account"
+            element={<Account user={user} setUserId={setUserId} />}
+          />
+          <Route
+            path="/carts/user/:id"
+            element={
+              <Cart
+                token={token}
+                userId={userId}
+                products={products}
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+              />
+            }
+          />
+          <Route
+            path="/products/:id"
+            element={<SingleProduct token={token} />}
+          />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    <BrowserRouter>
-    <nav>
-      <Nav token={token} setToken={setToken} userId={userId}/>
-    </nav>
-      <Routes>
-      <Route path="/" element={<Home token={token} setProducts={setProducts} cartItems={cartItems} setCartItems={setCartItems}/>}/>
-      <Route path="/register" element={<Register setId={setId}/>} />
-      <Route path="/login" element={<Login setToken={setToken} setUser={setUser} />} />
-      <Route path="/account" element={<Account user={user} setUserId={setUserId}/>} />
-      <Route path="/carts/user/:id" element={<Cart token={token} userId={userId} products={products} cartItems={cartItems} setCartItems={setCartItems}/>} />
-      <Route path="/products/:id" element={<SingleProduct token={token}/>} />
-      <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  )
+  );
 }
 
 export default App;
--- ecommerce-site/src/components/Account.jsx
+++ ecommerce-site/src/components/Account.jsx
@@ -4,44 +4,56 @@ import { useFetchUsersQuery } from "../redux/api";
 import { useNavigate } from "react-router-dom";
 
 function Account(props) {
-    const navigate = useNavigate();
-    
-    //fetch all users
-    const {data, error, isLoading} = useFetchUsersQuery();
-    console.log("Data", data);
-    console.log("Error", error);
-    console.log("isLoading", isLoading);
+  const navigate = useNavigate();
 
+  //fetch all users
+  const { data, error, isLoading } = useFetchUsersQuery();
+  console.log("Data", data);
+  console.log("Error", error);
+  console.log("isLoading", isLoading);
 
-    if(isLoading) {
-        return <div>Loading Account Details...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading Account Details...</div>;
+  }
 
-    if(error) {
-        return <div>Error</div>;
-    }
-    
-    //set userId state with matched user
-    const result = data.find(({ username }) => username === props.user);
-    // console.log(props);
-    // console.log(result.id);
-    props.setUserId(result.id);
-    
-    if(result.id) {
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  //set userId state with matched user
+  const result = data.find(({ username }) => username === props.user);
+  // console.log(props);
+  // console.log(result.id);
+  props.setUserId(result.id);
+
+  if (result.id) {
     return (
-            <>
-            <h2>Account Details</h2>
-            <p><strong>Name</strong>: {result.name.firstname} {result.name.lastname} </p>
-            <p><strong>Email</strong>: {result.email}</p>
-            <p><strong>Address</strong>: {result.address.number} {result.address.street}, {result.address.city}, {result.address.zipcode}</p>
-            <p><strong>Phone</strong>: {result.phone}</p>
-            <br />
-            <button onClick={() => navigate(`/`)}>Return to Shop</button><br/>
-            <br />
-            <button onClick={() => navigate(`/carts/user/${result.id}`)}>View Your Cart</button>
-            </>
-        );
-      } 
+      <>
+        <h2>Account Details</h2>
+        <p>
+          <strong>Name</strong>: {result.name.firstname} {result.name.lastname}{" "}
+        </p>
+        <p>
+          <strong>Email</strong>: {result.email}
+        </p>
+        <p>
+          <strong>Address</strong>: {result.address.number}{" "}
+          {result.address.street}, {result.address.city},{" "}
+          {result.address.zipcode}
+        </p>
+        <p>
+          <strong>Phone</strong>: {result.phone}
+        </p>
+        <br />
+        <button onClick={() => navigate(`/`)}>Return to Shop</button>
+        <br />
+        <br />
+        <button onClick={() => navigate(`/carts/user/${result.id}`)}>
+          View Your Cart
+        </button>
+      </>
+    );
+  }
 }
 
 export default Account;
